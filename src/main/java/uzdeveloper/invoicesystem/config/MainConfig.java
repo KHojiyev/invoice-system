3a86c9abe3189d3ac936460111f6066949bd65f3
user <1705serafin@gmail.com>
2021-06-06 12:49:36 +0500

    swagger2 added to config class

---
+++ src/main/java/uzdeveloper/invoicesystem/config/MainConfig.java
@@ -0,0 +1,29 @@
+package uzdeveloper.invoicesystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class MainConfig {
+
+    @Bean
+    public Docket api(){
+return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("uzdeveloper.invoicesystem.controller"))
+        .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+return new ApiInfoBuilder().title("Main Controller").build();
+    }
+
+
+}
